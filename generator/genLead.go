f5ecba6ce870681a94279a9606fe5e371e1832dd
Francis Stephens <francisstephens@gmail.com>
2016-01-17 21:27:43 +0000

    Adding chord changes to lead sessions

--- generator/genLead.go
+++ generator/genLead.go
@@ -18,12 +18,21 @@ type leadSession struct {
 	Classical7    string
 	Classical8    string
 	ClassicalLong string
+	ChordChanges1 string
+	ChordChanges2 string
+	ChordChanges3 string
+	ChordChanges4 string
+	ChordChanges5 string
+	ChordChanges6 string
+	ChordChanges7 string
+	ChordChanges8 string
 }
 
 func generateLead() {
 	riffs := getTitles("lead/riffs.json")
 	classical := getTitles("lead/classical.json")
 	classicalLong := getTitles("lead/classicalLong.json")
+	chordChanges := getTitles("lead/chordChanges.json")
 	sds := make([]leadSession, 7)
 	for i := range sds {
 		sds[i].Riff1 = riffs.next()
@@ -39,6 +48,14 @@ func generateLead() {
 		sds[i].Classical7 = classical.next()
 		sds[i].Classical8 = classical.next()
 		sds[i].ClassicalLong = classicalLong.next()
+		sds[i].ChordChanges1 = chordChanges.next()
+		sds[i].ChordChanges2 = chordChanges.next()
+		sds[i].ChordChanges3 = chordChanges.next()
+		sds[i].ChordChanges4 = chordChanges.next()
+		sds[i].ChordChanges5 = chordChanges.next()
+		sds[i].ChordChanges6 = chordChanges.next()
+		sds[i].ChordChanges7 = chordChanges.next()
+		sds[i].ChordChanges8 = chordChanges.next()
 	}
 	for i := range sds {
 		applyTemplate(sds[i], "lead", time.Weekday(i))
