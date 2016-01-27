9feded5a59ff6e1789161462346e785c2a2585f2
Francis Stephens <francisstephens@gmail.com>
2016-01-27 21:32:40 +0000

    Templates now pull in their own data
    A variable name in a template indicates the name of the file where the
    generator can find values to fill that variable. Removes the need to
    tedious genLead/Rhythm.go files.

--- generator/main.go
+++ generator/main.go
@@ -6,8 +6,80 @@ import (
 	"os"
 	"strings"
 	"text/template"
+	"text/template/parse"
 	"time"
 )
+
+func main() {
+	applyTemplate("rhythm")
+	applyTemplate("lead")
+}
+
+func applyTemplate(category string) string {
+	bytes, err := ioutil.ReadFile(category + "/template.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	t := template.Must(template.New("session").Parse(string(bytes)))
+	tMan := newTitleManager(t, category)
+	for d := time.Sunday; d <= time.Saturday; d++ {
+		tMan.makeValues()
+		err = os.MkdirAll("../practice/"+category+"/", 0777)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fd, err := os.Create("../practice/" + category + "/" + d.String() + ".json")
+		if err != nil {
+			log.Fatal(err)
+		}
+		err = t.Execute(fd, tMan.varMap)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return ""
+}
+
+type titleManager struct {
+	t        *template.Template
+	category string
+	titleMap map[string]*titles
+	varMap   map[string]string
+}
+
+func newTitleManager(t *template.Template, category string) *titleManager {
+	return &titleManager{
+		t:        t,
+		category: category,
+		titleMap: make(map[string]*titles),
+		varMap:   make(map[string]string),
+	}
+}
+
+func (m *titleManager) makeValues() {
+	m.varMap = make(map[string]string)
+	for _, n := range m.t.Root.Nodes {
+		if n.Type() == parse.NodeAction {
+			m.makeValue(n.String(), m.category)
+		}
+	}
+}
+
+func (m *titleManager) makeValue(templateVar, category string) {
+	varName := strings.Trim(templateVar, "{}.")
+	if _, ok := m.varMap[varName]; ok {
+		return
+	}
+	titleName := strings.TrimRight(varName, "0123456789")
+	if titles, ok := m.titleMap[titleName]; ok {
+		m.varMap[varName] = titles.next()
+		m.makeValue(templateVar, category)
+		return
+	}
+	titles := getTitles(category + "/" + titleName + ".txt")
+	m.titleMap[titleName] = titles
+	m.makeValue(templateVar, category)
+}
 
 type titles struct {
 	idx    int
@@ -18,11 +90,6 @@ func (t *titles) next() string {
 	title := t.Titles[t.idx%len(t.Titles)]
 	t.idx++
 	return title
-}
-
-func main() {
-	generateRhythm()
-	generateLead()
 }
 
 func getTitles(fileName string) *titles {
@@ -45,25 +112,4 @@ func removeEmpty(ss []string) []string {
 		}
 	}
 	return nss
-}
-
-func applyTemplate(sessionData interface{}, category string, dow time.Weekday) string {
-	bytes, err := ioutil.ReadFile(category + "/template.json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	t := template.Must(template.New("session").Parse(string(bytes)))
-	err = os.MkdirAll("../practice/"+category+"/", 0777)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fd, err := os.Create("../practice/" + category + "/" + dow.String() + ".json")
-	if err != nil {
-		log.Fatal(err)
-	}
-	err = t.Execute(fd, sessionData)
-	if err != nil {
-		log.Fatal(err)
-	}
-	return ""
 }
