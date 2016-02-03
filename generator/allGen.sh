ee617d7b6f630770b2dcfc4a84c8a2de92b63d33
Francis Stephens <francisstephens@gmail.com>
2016-02-03 08:50:49 +0000

    Generating short_rhythm from rhythm data

--- generator/allGen.sh
+++ generator/allGen.sh
@@ -3,4 +3,4 @@
 go build
 ./generator -i lead -t template.json -o ../practice/lead
 ./generator -i rhythm -t template.json -o ../practice/rhythm
-./generator -i short_rhythm -t template.json -o ../practice/short_rhythm
+./generator -i rhythm -t short_template.json -o ../practice/short_rhythm
