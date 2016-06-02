c9eefde0507d3153de0cd5bd400a50458041f0cb
Francis Stephens <francisstephens@gmail.com>
2016-06-02 03:34:30 -0500

    Removing short rhythm session

--- generator/allGen.sh
+++ generator/allGen.sh
@@ -2,5 +2,4 @@
 
 go build
 ./generator -i lead -t template.json -o ../practice/lead
-./generator -i rhythm -t long_template.json -o ../practice/rhythm
-./generator -i rhythm -t short_template.json -o ../practice/short_rhythm
+./generator -i rhythm -t template.json -o ../practice/rhythm
