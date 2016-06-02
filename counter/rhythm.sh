1e1ffda5833f70042ce985d5542fbaf72a2cd956
Francis Stephens <francisstephens@gmail.com>
2016-06-02 03:44:15 -0500

    Split rhythm playing into two shorter sessions

--- counter/rhythm.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-go build && ./counter -dow ../practice/rhythm/
--- generator/allGen.sh
+++ generator/allGen.sh
@@ -2,4 +2,5 @@
 
 go build
 ./generator -i lead -t template.json -o ../practice/lead
-./generator -i rhythm -t template.json -o ../practice/rhythm
+./generator -i rhythm1 -t template.json -o ../practice/rhythm1
+./generator -i rhythm2 -t template.json -o ../practice/rhythm2
