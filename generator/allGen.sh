5d1bde7418cc97ef3c43d0bef23366fcb3496b9b
Francis Stephens <francisstephens@gmail.com>
2017-01-17 13:17:58 +0100

    Adding ukelele practice

--- generator/allGen.sh
+++ generator/allGen.sh
@@ -3,3 +3,4 @@
 go build
 ./generator -i lead -t template.json -o ../practice/lead
 ./generator -i rhythm -t template.json -o ../practice/rhythm
+./generator -i ukelele -t template.json -o ../practice/ukulele
